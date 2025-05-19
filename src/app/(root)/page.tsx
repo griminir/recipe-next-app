bb4cf415d0f34c43bcf93806ad23def6b1c718e7
Viktor E. Degray <viktor.elliot.king@gmail.com>
2025-05-19 14:23:59 +0200

    Add SearchForm and SearchFormReset components; update Home component for recipe search functionality

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,23 @@
-export default function Home() {
+import SearchForm from '@/components/SearchForm';
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
     <>
-      <section className='pink_container'>
-        <h1 className='heading'>Home</h1>
+      <section className='green_container'>
+        <h1 className='heading'>
+          Your familes recipes <br /> all in one place
+        </h1>
+
+        <p className='sub-heading !max-w-3x'>
+          Add recipes, share recipes, make the world a giant family cookbook
+        </p>
+
+        <SearchForm query={query} />
       </section>
     </>
   );
