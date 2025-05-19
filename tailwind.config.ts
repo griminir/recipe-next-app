1d7c40fdc80f2bbf16a4e3082be7d5c4fb74d008
Viktor E. Degray <viktor.elliot.king@gmail.com>
2025-05-19 13:44:20 +0200

    Add components.json configuration, update package dependencies, and enhance global styles with utility classes
    also think i have resloved the css issues between tailwind 3.x and tailwind 4 as wells as i think i have installed shadcn correctly for a next 15 + react 19 project "hope and dreams"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,10 +5,10 @@ import typography from '@tailwindcss/typography';
 const config: Config = {
   darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/sanity/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages//*.{js,ts,jsx,tsx,mdx}',
+    './src/components//.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/.{js,ts,jsx,tsx,mdx}',
+    './src/sanity/*/.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
